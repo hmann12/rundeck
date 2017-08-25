58857c3e62e371e524c5e63b249db68093d9cbd3
hmann12 <harshdeepm@julep.com>
2017-08-25 13:28:14 -0700

    made some changes to the script

--- rundeck_cli.py
+++ rundeck_cli.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
+from __future__ import print_function
+from __future__ import print_function
+from __future__ import print_function
+from __future__ import print_function
 import argparse
 import json
 from subprocess import Popen, PIPE
 from datetime import datetime
 
-log_file_name = '/ebs/logs/nginx/rundeck-cleanup.log'
+log_file_name = 'rundeck-cleanup.log'
 log_file_mode = 'a+'
 parser = argparse.ArgumentParser(description='input number of days')
 parser.add_argument('--remove_older_than', required=True)
 args = parser.parse_args()
 f = open(log_file_name, log_file_mode)
+guard = 100
 
 
 def get_projects():
@@ -25,13 +30,17 @@ def get_projects():
 def delete_executions(project_names_list):
     no_errors = True
     for project_name in project_names_list:
-        cmd = ('rd executions deletebulk --older {} --project {} -y'.format(args.remove_older_than, project_name))
-        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-        p.wait()
-        output, error = p.communicate()
-        if p.returncode != 0:
-            f.write(str(datetime.now()) + ' Execution deletion failed for {} - '.format(project_name) + error + '\n')
-            no_errors = False
+        for i in xrange(guard):
+            cmd = ('rd executions deletebulk --older {} --project {} -y'.format(args.remove_older_than, project_name))
+            p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            p.wait()
+            output, error = p.communicate()
+            if p.returncode != 0:
+                f.write(str(datetime.now()) + ' Execution deletion failed for {} - '.format(project_name) + error + '\n')
+                no_errors = False
+                break
+            if output == 'No executions found to delete':
+                break
     return no_errors
 
 
@@ -40,14 +49,14 @@ try:
     if type(projects) is list:
         x = delete_executions(projects)
         if x:
-            print json.dumps({'status': 'success'})
+            print(json.dumps({'status': 'success'}))
         else:
-            print json.dumps({'status': 'one or more projects failed to have their executions deleted, check logs'})
+            print(json.dumps({'status': 'one or more projects failed to have their executions deleted, check logs'}))
     else:
-        print json.dumps({'status': 'error', 'message': 'Could not retrieve project list'})
+        print(json.dumps({'status': 'error', 'message': 'Could not retrieve project list'}))
         f.write(str(datetime.now()) + ' Could not retrieve project list' + '\n')
 except Exception as ex:
-    print json.dumps({'status': 'error', 'message': 'execution deletion failed, Exception Error, check logs'})
+    print(json.dumps({'status': 'error', 'message': 'execution deletion failed, Exception Error, check logs'}))
     f.write(str(datetime.now()) + ' Execution deletion failed: ' + str(ex) + '\n')
 finally:
     f.close()
